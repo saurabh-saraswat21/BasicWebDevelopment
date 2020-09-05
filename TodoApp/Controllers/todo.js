e098874637d9f4d15330f4df8862ae7a9cc1255b
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-05 20:13:27 +0530

    add edit and delete functionality

--- TodoApp/Controllers/todo.js
+++ TodoApp/Controllers/todo.js
@@ -1,27 +1,55 @@
-const  Mongoose  = require('mongoose');
+const Mongoose = require('mongoose');
 
-module.exports=(app)=>{
+module.exports = (app) => {
     const models = require('../Models/toDoDatabase')
-    const todoModal = Mongoose.model('todos',models.todoSchema)
+    const todoModal = Mongoose.model('todos', models.todoSchema)
 
-    app.get('/',(req,res)=>{
-        todoModal.find({} ,(err,data)=>{
+    app.get('/', (req, res) => {
+        todoModal.find({}, (err, data) => {
 
-            res.render('todo',{todos:data});
+            res.render('todo', { todos: data });
         })
     })
-    app.post('/', (req,res)=>{
+    app.post('/', (req, res) => {
 
-        var newtodo = todoModal({todo:req.body.content})
+        var newtodo = todoModal({ todo: req.body.content })
         newtodo.save().then((result) => {
             console.log("new todo created")
             res.redirect('/')
         })
     })
-    app.post('/delete',(req,res)=>{
+    app.post('/delete', (req, res) => {
         console.log(req.body)
         res.send("hello")
-        
+
     })
+
+
+
+
+    app
+        .route("/edit/:id")
+        .get((req, res) => {
+            const id = req.params.id;
+            todoModal.find({}, (err, result) => {
+                res.render("todoUpdate.ejs", { todos: result, idTask: id });
+            });
+        })
+        .post((req, res) => {
+            const id = req.params.id;
+            todoModal.findByIdAndUpdate(id, { todo: req.body.content }, err => {
+                if (err) return res.send(500, err);
+                res.redirect("/");
+            });
+        });
+
+
+    app.route("/remove/:id").get((req, res) => {
+        const id = req.params.id;
+        todoModal.findByIdAndRemove(id, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
 
 }
