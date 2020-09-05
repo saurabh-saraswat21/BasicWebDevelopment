ded65ab05c1abb7b9ce993db88a54be31c5f1d67
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-05 16:14:36 +0530

    add dyamic data from backend

--- TodoApp/Controllers/todo.js
+++ TodoApp/Controllers/todo.js
@@ -1,8 +1,32 @@
+var list =[
+    
+        {item:'todo1'},
+        {
+
+            item:'todo2'
+        },
+        {
+
+            item:'todo3'
+        },
+        {
+
+            item:'todo4'
+        }
+
+    
+]
+
+
+
 module.exports=(app)=>{
 
     app.get('/',(req,res)=>{
-        res.render('todo');
+        res.render('todo',{todolist:list});
         console.log("homepage loaded")
+    })
+    app.post('/additem',(req,res)=>{
+
     })
 
 }
