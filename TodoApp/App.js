66d17feb74de852a957a13fd4fe3a0eb64d51655
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-05 17:55:47 +0530

    connection to databse

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -21,8 +21,17 @@ app.set('view engine','ejs')
 
 app.use(bodyParser.urlencoded({extended:true}))
 
+mongoose.connect('mongodb://localhost:27017/to-do-App', { useNewUrlParser: true, useUnifiedTopology: true })
+
 //using controllers
 todoController(app);
+
+
+mongoose.connection.once('open', () => {
+    console.log('connection made');
+}).on('err', (err) => {
+    console.log(err);
+})
 
 //listen server
 app.listen(port,()=>{
